8d71675a0e95bd3a8927c3c195b78c68e455a1ce
Statyx <julien.meichelbeck@gmail.com>
2015-09-29 16:24:22 +0200

    Cleanup (minor)

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -3,11 +3,11 @@ class Rank < ActiveRecord::Base
   belongs_to :team, required: true
 
   def game_count
-    @game_count ||= self.win+self.loss+self.tie+self.win_ot+self.loss_ot
+    @game_count ||= self.win + self.loss + self.tie + self.win_ot + self.loss_ot
   end
 
   def points
-    @points ||= self.win*3+self.tie+self.win_ot*2+self.loss_ot
+    @points ||= self.win*3 + self.tie + self.win_ot*2 + self.loss_ot
   end
 
 end
