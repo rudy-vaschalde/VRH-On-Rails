904aab887f2579cc51480d726c258202223cfe9d
Statyx <julien.meichelbeck@gmail.com>
2015-10-01 16:50:42 +0200

    Fix goal.rb

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,20 +1,15 @@
 class Goal < ActiveRecord::Base
   belongs_to :game, required: true
-
   belongs_to :scorer, class_name: 'Player'
   belongs_to :passer, class_name: 'Player'
-
   belongs_to :team, required: true
 
-  validate :belongs_to_team, on: :create
- 
+  validate :belongs_to_team, on: :create
 
   private
-
-  # If the player is set for a goal, check that he belongs to the given team!
-  # Only on creation, because he can change team in the future
-  def belongs_to_team
-    errors.add(:player_id, "ne fait pas parti de l'équipe") unless self.player.nil? || self.player.team == self.team
-  end
-
+    # If the player is set for a goal, check that he belongs to the given team!
+    # Only on creation, because he can change team in the future
+    def belongs_to_team
+       errors.add(:player_id, "ne fait pas parti de l'équipe") unless self.player.nil? || self.player.team == self.team
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 for i in 1..5 do
   Team.create!(name: "Team #{i}", city: "City #{i}")
 end
