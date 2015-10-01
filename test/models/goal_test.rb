1d8723f8d5176a72c9eed9321e89cb85fc0e13dc
Statyx <julien.meichelbeck@gmail.com>
2015-10-01 16:23:28 +0200

    Fix testcase, oops.

--- test/models/goal_test.rb
+++ test/models/goal_test.rb
@@ -8,18 +8,18 @@ class GoalTest < ActiveSupport::TestCase
     @other_team = teams(:two)
   end
 
-  test "should create goal when player belongs to team"
+  test "should create goal when player belongs to team" do
     goal = Goal.create(player: @player, team: @player.team, game: @game)
     assert goal.valid?
   end
 
-  test "should not create goal when player doesn't belong to the team"
+  test "should not create goal when player doesn't belong to the team" do
     goal = Goal.create(player: @player, team: @other_team, game: @game)
     assert_not goal.valid?
     assert_equal goal.errors[:player_id], "ne fait pas parti de l'équipe"
   end
 
-  test "should create goal when player is not set"
+  test "should create goal when player is not set" do
     goal = Goal.create(team: @player.team, game: @game)
     assert goal.valid?
   end
