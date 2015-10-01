5b78cad47d5c21dc5a7def6c42ab68f3148afda2
Statyx <julien.meichelbeck@gmail.com>
2015-10-01 16:21:45 +0200

    Check player/team relationship when creating a goal (WITH TESTCASE)

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -7,4 +7,15 @@ class Goal < ActiveRecord::Base
 
   belongs_to :team, through: :scorer
 
+  validate :belongs_to_team, on: :create
+ 
+
+  private
+
+  # If the player is set for a goal, check that he belongs to the given team!
+  # Only on creation, because he can change team in the future
+  def belongs_to_team
+    errors.add(:player_id, "ne fait pas parti de l'équipe") unless self.player.nil? || self.player.team == self.team
+  end
+
 end
--- test/models/goal_test.rb
+++ test/models/goal_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class GoalTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @game = games(:one)
+    @player = players(:one)
+    @other_team = teams(:two)
+  end
+
+  test "should create goal when player belongs to team"
+    goal = Goal.create(player: @player, team: @player.team, game: @game)
+    assert goal.valid?
+  end
+
+  test "should not create goal when player doesn't belong to the team"
+    goal = Goal.create(player: @player, team: @other_team, game: @game)
+    assert_not goal.valid?
+    assert_equal goal.errors[:player_id], "ne fait pas parti de l'équipe"
+  end
+
+  test "should create goal when player is not set"
+    goal = Goal.create(team: @player.team, game: @game)
+    assert goal.valid?
+  end
+
 end
