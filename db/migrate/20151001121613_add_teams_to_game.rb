3b0cab1967045fb1b1d4b473c534d489e35469cf
Statyx <julien.meichelbeck@gmail.com>
2015-10-05 16:58:39 +0200

    Try to fix FK issues

--- db/migrate/20151001121613_add_teams_to_game.rb
+++ db/migrate/20151001121613_add_teams_to_game.rb
@@ -2,7 +2,7 @@ class AddTeamsToGame < ActiveRecord::Migration
   def change
     add_reference :games, :home_team, index: true
     add_reference :games, :visitor_team, index: true
-    add_foreign_key :games, :teams, column: :home_team_id, primary_key: :team_id
-    add_foreign_key :games, :teams, column: :visitor_team_id, primary_key: :team_id
+    add_foreign_key :games, :teams, column: :home_team_id, primary_key: :id
+    add_foreign_key :games, :teams, column: :visitor_team_id, primary_key: :id
   end
 end
--- db/migrate/20151001135049_add_players_to_goals.rb
+++ db/migrate/20151001135049_add_players_to_goals.rb
@@ -2,8 +2,8 @@ class AddPlayersToGoals < ActiveRecord::Migration
   def change
     add_reference :goals, :scorer, index: true
     add_reference :goals, :passer, index: true
-    add_foreign_key :goals, :players, column: :scorer_id, primary_key: :player_id
-    add_foreign_key :goals, :players, column: :passer_id, primary_key: :player_id
+    add_foreign_key :goals, :players, column: :scorer_id, primary_key: :id
+    add_foreign_key :goals, :players, column: :passer_id, primary_key: :id
 
   end
 end
