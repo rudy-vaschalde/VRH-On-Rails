a611b622a375637f2934d02baf1983c1480146f0
Julien Meichelbeck <julien.meichelbeck@gmail.com>
2015-12-10 13:25:13 -0700

    Fix testcase

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,9 +3,9 @@ class PublicController < ApplicationController
   # GET /public/index
   def index
     if Championship.n2.nil? || Championship.n4.nil?
-        @current_seasons = [nil, nil]
+        @current_seasons = []
     else
-      @current_seasons = [Championship.n2.current_season, Championship.n4.current_season]
+      @current_seasons = [Championship.n2.current_season, Championship.n4.current_season].compact
     end
     @articles = Article.featured
     @next_match = Game.next_n2
